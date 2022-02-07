39e16beda5e93495ba6892dd66a5eee19aeeb032
Alexa Amoriello <axa9517@rit.edu>
2022-02-07 15:47:12 -0500

    titlescreen and char
    aa

---
+++ CasualAnimals/Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// All Potential Options for game states
+public enum StateEnum { Title, Game, Pause, None };
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// Static fields are like global, should persist throughout the game load.
+    /// </summary>
+    [SerializeField] public static GameManager gameInstance;
+    [SerializeField] public static StateEnum currentState;
+    [SerializeField] public static StateEnum priorState;
+
+    /// <summary>
+    /// Need an Awake for instancing
+    /// </summary>
+    void Awake()
+    {
+        if (!gameInstance)
+        {
+            gameInstance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //Duplicate GameManager created every time the scene is loaded
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Sets the current and prior state to default values when loaded
+    /// </summary>
+    void Start()
+    {
+        // Set the current state to be the title screen
+        currentState = StateEnum.Title;
+        priorState = StateEnum.None;
+    }
+
+    /// <summary>
+    /// State machine for the game state
+    /// MANAGER MUST START OFF IN TITLESCREEN OR WILL BREAK
+    /// </summary>
+    void Update()
+    {
+        switch (GameManager.currentState)
+        {
+            case StateEnum.Title:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    // Store the prior state
+                    priorState = currentState;
+
+                    // Start the game
+                    currentState = StateEnum.Game;
+                    SceneManager.LoadScene("MainGame");
+                }
+                break;
+            case StateEnum.Game:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    // Pause game
+                    priorState = currentState;
+                    currentState = StateEnum.Pause;
+                }
+                break;
+            case StateEnum.Pause:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    // Unpause the game, basic
+                    priorState = currentState;
+                    currentState = StateEnum.Game;
+                }
+
+                //may put a menu here? WIP?
+                break;
+        }
+    }
+}
