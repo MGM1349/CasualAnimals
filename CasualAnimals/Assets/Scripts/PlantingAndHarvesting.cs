af6d13efc996a3ad454cb41514390bf7a58dadea
Marc Molnar <mgm1349@rit.edu>
2022-02-23 16:50:14 -0500

    Collision working correctly

--- CasualAnimals/Assets/Scripts/PlantingAndHarvesting.cs
+++ CasualAnimals/Assets/Scripts/PlantingAndHarvesting.cs
@@ -28,31 +28,9 @@ public class PlantingAndHarvesting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.E) && cropManager.fields[0].GetCropAtFieldPosition(0) == null)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                cropManager.CreateCropToField(0, i);
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.E) && cropManager.fields[0].GetCropAtFieldPosition(0).harvestable)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                amountHarvested += cropManager.RemoveCropToField(0, i);
-            }
-        }
-
-        eggplantsHarvested.text = amountHarvested.ToString();*/
-
-        if(playerScript.CurrentCropStand == -1)
-        {
-            //do nothing
-        }
-
         if (Input.GetKeyDown(KeyCode.E))
         {
-            int[] fieldPosition = { playerScript.CurrentFieldStand, playerScript.CurrentFieldStand };
+            int[] fieldPosition = { playerScript.CurrentFieldStand, playerScript.CurrentCropStand  };
 
             if(fieldPosition[0] == noCrop || fieldPosition[1] == noCrop)
             {
@@ -63,49 +41,13 @@ public class PlantingAndHarvesting : MonoBehaviour
             if(cropManager.fields[fieldPosition[0]].GetComponent<Field>().GetCropAtFieldPosition(fieldPosition[1]) == null)
             {
                 cropManager.CreateCropToField(fieldPosition[0], fieldPosition[1]);
-                //cropPosGrowing.Add(fieldPosition[1]);
             }
             else if(cropManager.fields[fieldPosition[0]].GetComponent<Field>().GetCropAtFieldPosition(fieldPosition[1]).harvestable)
             {
                 amountHarvested += cropManager.RemoveCropToField(fieldPosition[0], fieldPosition[1]);
-                //int index = cropPosGrowing.IndexOf(playerScript.CurrentCropStand);
-                //cropPosGrowing.RemoveAt();
             }
         }
 
         eggplantsHarvested.text = amountHarvested.ToString();
     }
-
-    /*public int[] CheckCollisionWithField()
-    {
-        BoxCollider2D playerCollider = player.GetComponent<BoxCollider2D>();
-
-        for (int i = 0; i < cropManager.fields.Count; i++)
-        {
-            Field currentField = cropManager.fields[i].GetComponent<Field>();
-
-            if (cropManager.fields[i].GetComponent<BoxCollider2D>().IsTouching(playerCollider)){
-                for (int j = 0; j < currentField.cropImages.Count; j++)
-                {
-                    if (currentField.cropImages[j].GetComponent<BoxCollider2D>().IsTouching(playerCollider))
-                    {
-                        int[] fieldPosition = { i, j };
-                        return fieldPosition;
-                    }
-                }
-            }
-        }
-
-        return null;
-    }*/
-
-/*    public bool CheckCollision(GameObject fieldPosition)
-    {
-        BoxCollider2D playerCollider = player.GetComponent<BoxCollider2D>();
-        BoxCollider2D fieldCollider = fieldPosition.GetComponent<BoxCollider2D>();
-
-
-
-        return false;
-    }*/
 }
